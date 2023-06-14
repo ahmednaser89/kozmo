24211d036a2bf858a96b2e0e499b19a03f24aa0a
ahmednaser89 <ahmednaser89@users.noreply.github.com>
2023-06-14 16:06:57 +0300

    Create thunkdis.js

---
+++ thunkdis.js
@@ -0,0 +1,22 @@
+<script src="https://thunkable.github.io/webviewer-extension/thunkableWebviewerExtension.js" type="text/javascript"></script>
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
+ThunkableWebviewerExtension.receiveMessage(function(message) {
+  // Do something with your message
+  alert(message)
+});
